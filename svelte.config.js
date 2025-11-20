016f2a08c22814583f59439bdf4d5fec00e7cce3
rainmodred <albphoenix@gmail.com>
2025-11-20 21:26:30 +0300

    setup deploy

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,10 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		paths: { base: process.argv.includes('dev') ? '' : '/pathfinder' }
+	}
 };
 
 export default config;
